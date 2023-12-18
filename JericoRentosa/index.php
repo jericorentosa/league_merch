6967cdcc8d7fd5016c0969b2a40dfbc8c27a8cd9
Jerico Rentosa <143145317+jericorentosa@users.noreply.github.com>
2023-12-18 22:03:57 +0800

    Add files via upload

---
+++ JericoRentosa/index.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>My Website</title>
+   <!-- add icon link -->
+        <link rel = "icon" href ="https://img.icons8.com/?size=512&id=115635&format=png" 
+        type = "image/x-icon">
+</head>
+
+<body>
+  <!-- Header -->
+  <section id="header">
+    <div class="header container">
+      <div class="nav-bar">
+        <div class="brand">
+          <a href="">
+            <h1><span>J</span>erico <span>R</span>entosa</h1>
+          </a>
+        </div>
+        <div class="nav-list">
+          <div class="hamburger">
+            <div class="bar"></div>
+        </div>
+          <ul>
+            <li><a href="#hero" data-after="Home">Home</a></li>
+            <li><a href="#projects" data-after="Projects">Projects</a></li>
+            <li><a href="#about" data-after="About">About</a></li>
+            <li><a href="#contact" data-after="Contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Header -->
+
+
+  <!-- Hero Section  -->
+  <section id="hero">
+    <div class="hero container">
+      <div>
+        <h1>Hello, <span></span></h1>
+        <h1>My Name is <span></span></h1>
+        <h1>JERICO <span></span></h1>
+        <a href="#projects" type="button" class="cta">Portfolio</a>
+      </div>
+    </div>
+  </section>
+  <!-- End Hero Section  -->
+
+  <!-- Projects Section -->
+  <section id="projects">
+    <div class="projects container">
+      <div class="projects-header">
+        <h1 class="section-title">Recent <span>Projects</span></h1>
+      </div>
+      <div class="all-projects">
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 1</h1>
+            <h2>Spike Bat</h2>
+            <p>"Spiked Bat" is an enthralling short animated film crafted through Blender, depicting the evolution of a seemingly ordinary baseball bat into a formidable weapon adorned with nails and thorns. This project aims to showcase the journey of transformation, resilience, and empowerment through visually stunning storytelling.</p>
+          </div>
+          <div class="project-img">
+            <img src="videos/0001-0200.gif" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 2</h1>
+            <h2>Ghost Shooter</h2>
+            <p>"Ghost Shooter" is an exhilarating animated sequence crafted in Adobe Photoshop, showcasing a hand transformed into a gun-like form, firing ethereal energy blasts to combat haunting apparitions. This project aims to blend surrealism with action, illustrating a unique confrontation between the spectral and the tangible through visually striking animation.</p>
+          </div>
+          <div class="project-img">
+            <img src="videos/Rentosa_4Activity.gif" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 3</h1>
+            <h2>Mike Sing</h2>
+            <p>"Mike Sing" is a delightful animated short video crafted using Maya, centered around the endearing character, Mike, inspired by the beloved monster from "Monster Inc." This project is dedicated to showcasing Mike's journey as he embraces his passion for singing, coupled with expressive lip-sync animation, providing a heartwarming and entertaining experience for viewers of all ages.</p>
+          </div>
+          <div class="project-img">
+            <img src="videos/Rentosa, Jerico_Lipsync.gif" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 4</h1>
+            <h2>RoboStride</h2>
+            <p>"RoboStride" is a meticulously crafted animation project in Maya, showcasing the dynamic and rhythmic movement of a robot through a captivating running cycle. This project aims to explore the technical and creative aspects of animating a robot's locomotion, emphasizing fluidity, precision, and attention to detail.</p>
+          </div>
+          <div class="project-img">
+            <img src="videos/Rentosa_Run Cycle.gif" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 5</h1>
+            <h2>RoboStride</h2>
+            <p>"RoboStride" is an innovative animation project executed in Maya, focusing on the creation of a captivating walk cycle for a futuristic and stylized robot character. This project aims to demonstrate the meticulous attention to detail, technical skill, and artistic creativity involved in animating a realistic and fluid walk cycle for a mechanized being.</p>
+          </div>
+          <div class="project-img">
+            <img src="videos/Rentosa_Jerico_Walk Cycle.gif" alt="img">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Projects Section -->
+
+  <!-- About Section -->
+  <section id="about">
+    <div class="about container">
+      <div class="col-left">
+        <div class="about-img">
+          <img src="img/jerico.jpg" alt="img">
+        </div>
+      </div>
+      <div class="col-right">
+        <h1 class="section-title">About <span>me</span></h1>
+        <h2>Student & Developer</h2>
+        <p>I am Jerico O. Rentosa, a 21-year-old third-year student at Bicol University-Polangui, pursuing a Bachelor of Science in Information Technology majoring in Animation.</p>
+        <p>My portfolio showcases a collection of my works in animation and development. It reflects my passion for creating immersive and visually captivating digital experiences. Whether it's crafting compelling narratives through animation or developing innovative software solutions, I am dedicated to pushing boundaries and delivering high-quality results.</p>
+        <a href="#" class="cta">Download Resume</a>
+      </div>
+    </div>
+  </section>
+  <!-- End About Section -->
+
+  <!-- Contact Section -->
+  <section id="contact">
+    <div class="contact container">
+      <div>
+        <h1 class="section-title">Contact <span>info</span></h1>
+      </div>
+      <div class="contact-items">
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/phone.png" /></div>
+          <div class="contact-info">
+            <h1>Phone</h1>
+            <h2>+639275459271</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/new-post.png" /></div>
+          <div class="contact-info">
+            <h1>Email</h1>
+            <h2>jericorentosa9@gmail.com</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/map-marker.png" /></div>
+          <div class="contact-info">
+            <h1>Address</h1>
+            <h2>Batang, Ligao City, Albay</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Contact Section -->
+
+  <!-- Footer -->
+  <section id="footer">
+    <div class="footer container">
+      <div class="brand">
+        <h1><span>J</span>erico <span>R</span>entosa</h1>
+      </div>
+      <h2>Your Partner in Developing</h2>
+      <div class="social-icon">
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/facebook-new.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/instagram-new.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/?size=512&id=-x2V8Q63DerT&format=png" /></a>
+        </div>
+      </div>
+      <p>Copyright © 2023 Jerico Rentosa. All rights reserved</p>
+    </div>
+  </section>
+  <!-- End Footer -->
+  <script src="./app.js"></script>
+</body>
+
+</html>
